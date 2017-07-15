3c3114f6fc04cd05a2512fb724e80f39b602cf6c
Jonathan Chan <jchchan91@gmail.com>
2017-07-15 15:58:45 -0700

    Add GTK2-Runtime to PATH

--- profile.cmd
+++ profile.cmd
@@ -17,6 +17,7 @@ SET PATH=%PATH%;C:\Program Files (x86)\Meld
 SET PATH=%PATH%;C:\Program Files\doxygen\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Graphviz2.38\bin
 SET PATH=%PATH%;C:\Program Files\KDiff3
+SET PATH=%PATH%;C:\Program Files\GTK2-Runtime Win64\bin
 
 REM Python stuff
 REM SET PYTHON27_PATH=C:\Python27
