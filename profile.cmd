0d31d41244cc4afa65d9cf69e767334a133d8b23
Jonathan Chan <jchchan91@gmail.com>
2017-10-14 01:10:28 -0700

    Remove some username specifics in profile.cmd

--- profile.cmd
+++ profile.cmd
@@ -1,16 +1,16 @@
 @echo off
 
+echo "Loading profile.cmd"
+
 color 0b
 
 REM Setting Common PATH variables
-SET PATH=%PATH%;C:\Users\jonchan\pkgs\bin
-SET PATH=%PATH%;C:\Program Files (x86)\Notepad++
+SET PATH=%PATH%;C:\Users\%USERNAME%\pkgs\bin
 SET PATH=%PATH%;C:\Program Files\Sublime Text 3
 SET PATH=%PATH%;C:\Program Files\Git\bin
 SET PATH=%PATH%;C:\Program Files\Git\usr\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin
-SET PATH=%PATH%;C:\build\boost_1_57_0\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Meld
 SET PATH=%PATH%;C:\Program Files\doxygen\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Graphviz2.38\bin
@@ -29,28 +29,18 @@ SET PATH=%PATH%;C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Scripts
 
 REM Useful utility commands
 DOSKEY igrep=findstr /psinc:$1 $2 $3 $4 $5
-DOSKEY gitlog=git log --pretty=format:"%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cr)" --graph
-DOSKEY npp=notepad++.exe $1
 DOSKEY sublime=start sublime_text.exe -n $*
 DOSKEY emacs=start emacs $*
-DOSKEY editprofile=sublime_text.exe -n C:\Users\%USERNAME%\AppData\Roaming\profile.cmd
-DOSKEY sourceprofile=call C:\Users\%USERNAME%\AppData\Roaming\profile.cmd
+DOSKEY editprofile=sublime_text.exe -n %APPDATA%\profile.cmd
+DOSKEY sourceprofile=call %APPDATA%\profile.cmd
 DOSKEY rmdir=rmdir /q/s $*
 DOSKEY clipp=echo|set /p=%CD%|clip
 DOSKEY less=less -i $*
 DOSKEY home=cd /d C:\users\%USERNAME%\AppData\Roaming\
 DOSKEY rcopy=robocopy /E /R:0 $*
 
-REM Spline tools
-DOSKEY plots=start python E:\aea\scripts\python\plot_spline.py -i $*
-DOSKEY render=python E:\aea\scripts\python\render.py -t $1 -s $2
-
 REM Add MSVC variables to run stuff like cl.exe from Command Line
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-
-REM ROS stuff
-DOSKEY hydro=call C:\opt\ros\hydro\x86\setup.bat
-DOSKEY fuerte=call C:\opt\ros\fuerte\x86\env.bat
 
 REM Useful SD commands
 DOSKEY objrt=cd /d $3:\win\$1.obj\$1.$2\analog
@@ -59,9 +49,11 @@ DOSKEY tstrt=cd /d $3:\win\$1.bin\$1.$2\test_automation_bins\Analog\bin
 DOSKEY sdb=E:\tools\sdb\sdb.exe $*
 
 REM Mount common network drives
-if NOT exist Z: net use Z: \\analogfs\private\Scratch\JONCHAN
-if NOT exist Y: net use Y: \\analogfs\private\AnalogPlat\analogsyndata
-if NOT exist X: net use X: \\analogfs\private\AnalogPlat
+if %COMPUTERNAME%==JONCHAN-DESKTOP (
+	if NOT exist Z: net use Z: \\analogfs\private\Scratch\JONCHAN
+	if NOT exist Y: net use Y: \\analogfs\private\AnalogPlat\analogsyndata
+	if NOT exist X: net use X: \\analogfs\private\AnalogPlat
+)
 
 REM Alias for running source depot razzle
 DOSKEY razzle=%APPDATA%\razzle.cmd $*
