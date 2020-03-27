66d5e06811fec1ad5d93bddc8bf93f9597ed66cb
Jonathan Chan <jchchan91@gmail.com>
2020-03-27 11:28:03 -0700

    Fix RunAsAdmin

--- windows/common.psm1
+++ windows/common.psm1
@@ -10,5 +10,11 @@ function Invoke-ScriptAsAdmin(
         else {
             Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs -ArgumentList $argumentList
         }
+
+        # Exit so that we don't re-run the script again after exiting this function
+        exit
+    }
+    else {
+        # Do nothing. Already admin.
     }
 }
